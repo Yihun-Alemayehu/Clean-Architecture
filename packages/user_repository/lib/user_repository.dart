e5c5ec009972878194f71ec4479cc42983ef3da2
Yihun Alemayehu <yankure01@gmail.com>
2023-12-05 10:56:19 +0300

    fdb-3 commit

--- packages/user_repository/lib/user_repository.dart
+++ packages/user_repository/lib/user_repository.dart
@@ -1,3 +1,6 @@
 library user_repository;
 
-export '../src/entities/entities.dart';
+export 'src/entities/entities.dart';
+export 'src/models/model.dart';
+export 'src/user_repo.dart';
+export 'src/firebase_user_repo.dart';
